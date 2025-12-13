76fd66f1d7dfa4e2c1997baf57d27b2f23f1a1ea
vanlu <2209120827@qq.com>
2025-12-14 01:13:46 +0800

    update: Creating tool states for your player

--- scenes/characters/player/idle_state.gd
+++ scenes/characters/player/idle_state.gd
@@ -21,9 +21,16 @@ func _on_physics_process(_delta : float) -> void:
 
 func _on_next_transitions() -> void:
 	GameInputEvents.movement_input()
-	
+
 	if GameInputEvents.is_movement_input():
 		transition.emit("Walking")
+
+	if GameInputEvents.is_player_using_tool() && player.current_tool == DataTypes.Tools.AxeWood:
+		transition.emit("Chopping")
+	if GameInputEvents.is_player_using_tool() && player.current_tool == DataTypes.Tools.TillGround:
+		transition.emit("Tilling")
+	if GameInputEvents.is_player_using_tool() && player.current_tool == DataTypes.Tools.WaterCrops:
+		transition.emit("Watering")
 
 func _on_enter() -> void:
 	pass
---
+++ scripts/globals/data_types.gd
@@ -0,0 +1,10 @@
+class_name DataTypes
+
+enum Tools{
+	None = 0,
+	AxeWood = 1,
+	TillGround = 2,
+	WaterCrops = 3,
+	PlantCorn = 4,
+	PlantTomato = 5
+}
